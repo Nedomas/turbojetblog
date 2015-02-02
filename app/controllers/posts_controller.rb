8c0656276958cea3d74309c332c17f34614d8d87
Domas <domas.bitvinskas@me.com>
2015-02-03 00:39:54 +0200

    Markdown posts are read and shown

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,15 @@
 class PostsController < ApplicationController
   def index
+    @posts = Dir.glob('public/posts/*.md').map do |file_path|
+      filename = Pathname.new(file_path).basename.to_s
+      _, dashed_date, dashed_title = filename.match(/(\d{4}-\d{2}-\d{2})-(.*).md/).to_a
+
+      OpenStruct.new(
+        title: dashed_title.titleize,
+        slug: dashed_title,
+        date: dashed_date,
+      )
+    end
   end
 
   def show
