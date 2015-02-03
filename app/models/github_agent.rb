b6d9605f8965f0ae8fc07fc392b4bfecd2e0a940
Domas <domas.bitvinskas@me.com>
2015-02-03 17:29:46 +0200

    Clarify blog upload instructions

--- app/models/github_agent.rb
+++ app/models/github_agent.rb
@@ -32,11 +32,19 @@ class GithubAgent
     end
 
     memoize def github
+      check_params!
+
       Github.new(basic_auth: "#{ENV['GH_USERNAME']}:#{ENV['GH_PASSWORD']}")
     end
 
     def login
       [ENV['GH_USERNAME'], ENV['GH_REPO']]
+    end
+
+    def check_params!
+      %w(GH_USERNAME GH_PASSWORD GH_REPO).each do |param|
+        raise "Missing ENV['#{param}']" unless ENV[param]
+      end
     end
   end
 end
